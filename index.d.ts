bc9a3665681dc9845a2000c1cd67d0d934911e67
nurodev <me@nuro.dev>
2021-03-11 00:23:55 +0000

    Added new TypeScript source

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import { SupabaseClient } from '@supabase/supabase-js'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $supabase: SupabaseClient
-  }
-}
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { Plugin } from 'vue';
+
+import type { App } from 'vue';
+import type { SupabaseClientOptions } from '@supabase/supabase-js';
+
+let client_instance: SupabaseClient;
+
+export interface VueSupabaseOptions {
+	key: string;
+	options?: SupabaseClientOptions;
+	url: string;
+};
+
+export const createSupabase = (options: VueSupabaseOptions): Plugin => {
+	return {
+		install(app: App) {
+			const {
+				key,
+				options: supabaseOptions = <SupabaseClientOptions>{},
+				url,
+			} = options;
+	
+			client_instance = createClient(url, key, supabaseOptions);
+			app.config.globalProperties.$supabase = client_instance;
+		}
+	}
+}
+
+export const useSupabase = (): SupabaseClient => client_instance;
+
+export default {
+	createSupabase,
+	useSupabase,
+}
