8d14ddb7475494984766dd98c26d9a19351f1efa
Sduu <gumedesduduzo@gmail.com>
2021-07-16 19:14:47 +0200

    use vue-demi

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 'use strict';
+import {install, inject, isVue2, isVue3} from "vue-demi"
 
 import { createClient } from '@supabase/supabase-js'
 
+export const supabaseKey = 'supabase';
+
+export function useSupabase(key = null) { 
+  return inject(key !== null ? key : supabaseKey);
+}
+
 export default {
-  install: function (Vue, options) {
+  install: function (app, options) {
     const supabase = createClient(options.supabaseUrl, options.supabaseKey, options.supabaseOptions)
 
-    Object.defineProperties(Vue.prototype, {
-      $supabase: {
-        get: function() {
-          return supabase;
-        },
-      },
-    });
+    app.provide(supabaseKey, supabase);
 
-    Vue.supabase = supabase;
+    if (isVue3){
+      app.config.globalProperties.$supabase = supabase;
+    } else {
+      Object.defineProperties(app.prototype, {
+        $supabase: {
+          get: function() {
+            return supabase;
+          },
+        },
+      });
+      app.supabase = supabase;
+    }
   }
 };
