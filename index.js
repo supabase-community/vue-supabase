3ef083e35f6ba150d34e2cb4e415fdbcc05a5b3b
Sduu <gumedesduduzo@gmail.com>
2021-07-25 00:46:12 +0200

    :alembic: add logging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-
-const { inject, isVue3 } = require('vue-demi');
+const {isVue2, isVue3 } = require('vue-demi');
+const { provide, inject } = isVue3 ? require("vue") : require("@vue/composition-api")
 
 const { createClient } = require('@supabase/supabase-js')
 
@@ -18,7 +18,12 @@ exports['default'] = {
   install: function (app, options) {
     const supabase = createClient(options.supabaseUrl, options.supabaseKey, options.supabaseOptions)
 
-    app.provide(supabaseKey, supabase);
+    if (provide) {
+      provide(supabaseKey, supabase);
+    }
+
+    console.log('is vue 3',isVue3)
+    console.log('is vue 2', isVue2)
 
     if (isVue3){
       app.config.globalProperties.$supabase = supabase;
