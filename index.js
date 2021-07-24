a574837d3452180465821e45276f6dee768d24fd
Sduu <gumedesduduzo@gmail.com>
2021-07-25 01:36:17 +0200

    cleanup logging

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-const {isVue2, isVue3 } = require('vue-demi');
-const { provide, inject } = isVue3 ? require("vue") : require("@vue/composition-api")
+const { isVue3 } = require('vue-demi');
 
 const { createClient } = require('@supabase/supabase-js')
 
-const supabaseKey = 'supabase';
+const supabasesymbol = Symbol('supabase');
 
-function useSupabase(key = null) { 
-  return inject(key !== null ? key : supabaseKey);
+function useSupabase() { 
+  return inject(supabasesymbol);
 }
 
 
 exports['default'] = {
-  supabaseKey,
   useSupabase,
   install: function (app, options) {
     const supabase = createClient(options.supabaseUrl, options.supabaseKey, options.supabaseOptions)
 
-    if (provide) {
-      provide(supabaseKey, supabase);
-    }
-
-    console.log('is vue 3',isVue3)
-    console.log('is vue 2', isVue2)
-
     if (isVue3){
       app.config.globalProperties.$supabase = supabase;
+      app.provide(supabasesymbol, supabase);
     } else {
       Object.defineProperties(app.prototype, {
         $supabase: {
