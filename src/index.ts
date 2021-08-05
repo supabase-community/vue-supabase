7b0ea456aabf94e8c4667f1398e559777a1bf415
Sduu <gumedesduduzo@gmail.com>
2021-08-06 00:07:47 +0200

    Create copy of the origional index.js

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+const { isVue3, inject } = require('vue-demi');
+
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseSymbol = Symbol('supabase');
+
+/**
+ * Used to get the injected instance of SupabaseClient.
+ * If using Vuejs 2.x, this function will be undefined, please use
+ * `this.$supabase` instead.
+ * @returns SupabaseClient
+ */
+export function useSupabase() { 
+  return inject(supabaseSymbol);
+}
+
+export function install(app: any, options: any) {
+  const supabase = createClient(options.supabaseUrl, options.supabaseKey, options.supabaseOptions)
+
+  if (isVue3){
+    app.config.globalProperties.$supabase = supabase;
+    app.provide(supabaseSymbol, supabase);
+  } else {
+    Object.defineProperties(app.prototype, {
+      $supabase: {
+        get: function() {
+          return supabase;
+        },
+      },
+    });
+    app.supabase = supabase;
+  }
+}
